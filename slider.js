1235d33fe3808e41d1935fc720dabd4f0acc6ec5
Gozdehan <gozdehanozturk@gmail.com>
2016-06-09 01:09:51 +0300

    js kodları tamamlandı, hatalar düzeltilecek

--- slider.js
+++ slider.js
@@ -29,5 +29,68 @@ $(document).ready(function(){
            $(".slider-top").animate({
                   left:hedefLeft * -1}, 1000);
         }); 
+});
 
+$(document).ready(function(){
+  var click=0;
+  var allWidth=0;
+  var width=$(".slider-bottom ul li").width()*4;
+  var animasyon = false;
+
+  function animasyon2(px){
+  	$("slider-bottom ul").stop(false,false).animate({
+  		left:px*-1
+  	},1000);
+  }
+
+  $(".left-control").click(function(){
+    if(animasyon == true){
+    	return;
+    }
+    allWidth=click*width;
+    animasyon=true;
+     $($(".slider-bottom ul")[1]).detach().prependTo($($(".slider-bottom")))
+         $(".slider-bottom").css("left", "-812px");
+         $(".slider-bottom").animate({left: 0}, 1000, function (){
+           animasyon=false;
+         });
+         animasyon2(allWidth);
+         click++;
+  });
+
+});
+
+$(document).ready(function(){
+ var click = 0;
+ var allWidth = 0;
+ var width = $(".slider-bottom ul li").width()*4;
+ var animasyon=false;
+
+ function animasyon2(px){
+ 	$("slider-bottom ul").stop(false,false).animate({
+ 		left:px*-1
+ 	},1000);
+ }
+$(".right-control").click(function(){
+          if (true == animasyon) {
+            return;
+          }
+
+          allWidth=click*width;
+          animasyon = true;
+
+         if ($(".slider-bottom").css('left')!='-812px') {
+             $(".slider-bottom").animate({left: -812}, 1000, function () {
+              animasyon = false;
+             });
+          }
+         else {
+            $($(".slider-bottom ul")[0]).detach().appendTo($(".slider-bottom"));
+            $(".slider-bottom").css("left", "0px");
+            $(".slider-bottom").animate({left: -812}, 1000, function () {
+              animasyon = false;
+            });
+        }
+        click++;
+});
 });
